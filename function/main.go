5e832679e1868ac7843bd2a0f62162b9786a2ac6
shinstealer <39997731+Shinstealer@users.noreply.github.com>
2019-12-03 14:08:31 +0900

    shinstealer

--- function/main.go
+++ function/main.go
@@ -1,5 +1,20 @@
 package main
 
+func sum(nums ...int) (int, int) {
+	s := 0     // 합계
+	count := 0 // 요소 갯수
+	for _, n := range nums {
+		s += n
+		count++
+	}
+	return count, s
+}
+
 func main() {
+
+	count, total := sum(1, 2, 3, 4, 5)
+
+	println(count, total)
+
 	println("hello")
 }
